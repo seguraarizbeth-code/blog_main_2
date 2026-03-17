ab643fc493cdd93d9c7391d486dd229ea3fe20ef
seguraarizbeth <seguraarizbeth@gmail.com>
2026-03-17 11:17:13 -0700

    models.py

---
+++ blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Category(models,Model):
+    title = models.CharField(max_length=225)
+
+    class Meta:
+        ordering = ('title')
+        verbose_name_plural = 'Categories'
+
+    def _str_(self):
+        return self.title
+
+class Post(models.Model):
+
+    ACTIVE= 'active'
+    DRAFT = 'draft'
+
+    CHOICE_STATUS = {
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft')
+    }
+
+    Category = models.ForeignKey(Category, related_name= 'post', on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status =models.CharField(max_length=10,Choices=CHOICES_STATUS, default=ACTIVE)
+    image = models.ImageField(upload_to='upload/',blank=True,null=True)
+
+    def _str_(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name='comments',on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    email= models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.name
