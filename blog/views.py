092a5b818f64a2882dde23285cd4a77f030f8aec
seguraarizbeth <seguraarizbeth@gmail.com>
2026-03-23 18:01:41 -0700

    guardar infromacion relevante

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# -*- codigo: utf-8 -*-
+from django.db.models import Q
+from django.shortcuts import render,get_object_or_404,render
 
+
+from .models import Post,Category
 # Create your views here.
+def home(request):
+    posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
+    context ={
+        'post': posts
+    }
+    return render(request,'blog/home.htlm',context)
+
