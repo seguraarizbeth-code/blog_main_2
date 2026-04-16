2c3a4a1480a541713b0556e2615678f28c36cc10
seguraarizbeth <seguraarizbeth@gmail.com>
2026-04-16 16:59:24 -0700

    forms y views

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from .models import Comment
 
-class CommentFrom(forms.ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name','email','body']
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404, render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post, Category
+from .forms import CommentForm
+
 
 # Create your views here.
-def home(request):
+def home(request):  
+
     posts = Post.objects.filter(status=Post.ACTIVE).order_by('-created_at')
     context = {
         'posts': posts
@@ -15,6 +18,16 @@ def home(request):
 def detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.ACTIVE)
 
+    if request.method =='POST':
+        form = CommentForm(request.POST)
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post =post
+            comment.save()
+            
+            return redirect('post_detail')
+    
     context = {
         'post': post,
     }
